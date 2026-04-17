4139c703a9fbc994fb8ecc40a6c6a4a47b3fcb3e
Mehreen Butt <mehreenbutt984@gmail.com>
2026-04-17 08:04:25 +0500

    update backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     // Ab ye local se hat kar Vercel ke live server par connect hoga
-    baseURL: 'https://tutorhub-backend.vercel.app/api' 
+    baseURL: 'https://tutorhub-backend-production.up.railway.app/api' 
 });
 
 export default API;
